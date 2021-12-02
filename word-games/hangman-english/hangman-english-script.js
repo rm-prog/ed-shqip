a4faaede26a01a648f8cc7f66cbd2dbac642f5d6
Rminollari <rminollari@gmail.com>
2021-12-02 22:53:23 +0100

    fixed hangmanGame

--- word-games/hangman-english/hangman-english-script.js
+++ word-games/hangman-english/hangman-english-script.js
@@ -4,8 +4,7 @@ window.onload = function () {
           'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
           't', 'u', 'v', 'w', 'x', 'y', 'z'];
     
-    var categories;         // Array of topics
-    var chosenCategory;     // Selected catagory
+    var words;         // Array of topics
     var word ;              // Selected word
     var guess ;             // Geuss
     var geusses = [ ];      // Stored geusses
@@ -16,8 +15,6 @@ window.onload = function () {
     // Get elements
     var showLives = document.getElementById("mylives");
     var showClue = document.getElementById("clue");
-  
-  
   
     // create alphabet ul
     var buttons = function () {
@@ -34,18 +31,6 @@ window.onload = function () {
         letters.appendChild(list);
       }
     }
-      
-    
-    // Select Catagory
-    var selectCat = function () {
-      if (chosenCategory === categories[0]) {
-        catagoryName.innerHTML = "The Chosen Category Is Premier League Football Teams";
-      } else if (chosenCategory === categories[1]) {
-        catagoryName.innerHTML = "The Chosen Category Is Films";
-      } else if (chosenCategory === categories[2]) {
-        catagoryName.innerHTML = "The Chosen Category Is Cities";
-      }
-    }
   
     // Create geusses ul
      result = function () {
@@ -59,8 +44,10 @@ window.onload = function () {
         if (word[i] === "-") {
           guess.innerHTML = "-";
           space = 1;
-        } else {
+        } else if(i != 0) {
           guess.innerHTML = "_";
+        } else {
+            guess.innerHTML = word[0];
         }
   
         geusses.push(guess);
@@ -160,7 +147,7 @@ window.onload = function () {
         this.setAttribute("class", "active");
         this.onclick = null;
         for (var i = 0; i < word.length; i++) {
-          if (word[i] === geuss) {
+          if (word[i] === geuss && i != 0) {
             geusses[i].innerHTML = geuss;
             counter += 1;
           } 
@@ -179,25 +166,27 @@ window.onload = function () {
       
     // Play
     play = function () {
-      categories = [
-          ["everton", "liverpool", "swansea", "chelsea", "hull", "manchester-city", "newcastle-united"],
-          ["alien", "dirty-harry", "gladiator", "finding-nemo", "jaws"],
-          ["manchester", "milan", "madrid", "amsterdam", "prague"]
-      ];
-  
-      chosenCategory = categories[Math.floor(Math.random() * categories.length)];
-      word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+      words = ["television",
+      "attitude",
+      "product",
+      "newspaper",
+      "homework",
+      "connection",
+      "shopping",
+      "coffee",
+      "technology",
+      "responsibility"]
+      word = words[Math.floor(Math.random() * words.length)];
       word = word.replace(/\s/g, "-");
       console.log(word);
       buttons();
   
       geusses = [ ];
       lives = 10;
-      counter = 0;
+      counter = 1;
       space = 0;
       result();
       comments();
-      selectCat();
       canvas();
     }
   
@@ -208,14 +197,19 @@ window.onload = function () {
       hint.onclick = function() {
   
         hints = [
-          ["Based in Mersyside", "Based in Mersyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club"],
-          ["Science-Fiction horror film", "1971 American action film", "Historical drama", "Anamated Fish", "Giant great white shark"],
-          ["Northern city in the UK", "Home of AC and Inter", "Spanish capital", "Netherlands capital", "Czech Republic capital"]
+            "digital",
+            "behaviour",
+            "result of something",
+            "information source",
+            "boring",
+            "relation",
+            "buying",
+            "energy",
+            "smartphones",
+            "duty",
       ];
-  
-      var catagoryIndex = categories.indexOf(chosenCategory);
-      var hintIndex = chosenCategory.indexOf(word);
-      showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
+      var hintIndex = words.indexOf(word);
+      showClue.innerHTML = "Clue: - " +  hints[hintIndex];
     };
   
      // Reset
